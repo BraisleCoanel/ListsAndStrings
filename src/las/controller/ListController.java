e336443e7284d71bfd8bae68388a86848fc353c6
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-10-03 13:38:14 -0600

    Added a toString method to get the popup display to work.

--- src/las/controller/ListController.java
+++ src/las/controller/ListController.java
@@ -3,20 +3,33 @@ package las.controller;
 import java.util.List;
 import java.util.ArrayList;
 import las.model.Kahoot;
+import las.view.PopupDisplay;
 
 public class ListController
 {
 	private List<Kahoot> myKahoots;
+	private PopupDisplay popup;
+	
 	
 	public ListController()
 	{
 		myKahoots = new ArrayList<Kahoot>();
+		popup = new PopupDisplay();
 	}
 	
 	public void start()
 	{
 		Kahoot myFirstKahoot = new Kahoot();
 		myKahoots.add(myFirstKahoot);
+		fillTheList();
+		showTheList();
+	}
+	private void showTheList()
+	{
+		for (int index = 0; index < myKahoots.size(); index += 1)
+		{
+			popup.displayText(myKahoots.get(index).toString());
+		}
 	}
 	private void fillTheList()
 	{
