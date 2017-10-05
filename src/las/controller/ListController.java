711ed6e4be54130e53202c5068a3f38c5d40d4e7
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-10-05 13:18:12 -0600

    Changed construction parameters, and added a data member.

--- src/las/controller/ListController.java
+++ src/las/controller/ListController.java
@@ -26,18 +26,38 @@ public class ListController
 	}
 	private void showTheList()
 	{
+		String currentCreator = "";
 		for (int index = 0; index < myKahoots.size(); index += 1)
 		{
+			currentCreator = myKahoots.get(index).getCreator();
+			
+			Kahoot currentKahoot = myKahoots.get(index);
+			String creator = currentKahoot.getCreator();
+			
 			popup.displayText(myKahoots.get(index).toString());
+			
+			if (currentCreator.equals("nobody"))
+			{
+				for(int loop = 0; loop < 5; loop += 1)
+				{
+					popup.displayText("Wow nobody does a lot!");
+				}
+			}
+			
+			for (int currentLetterIndex = 0; currentLetterIndex < creator.length(); currentLetterIndex += 1)
+			{
+				popup.displayText(currentCreator.substring(currentLetterIndex, currentLetterIndex + 1));
+			}
+		
 		}
 	}
 	private void fillTheList()
 	{
-		Kahoot fiftyStates = new Kahoot("Kashish", 50);
-		Kahoot mySecondKahoot = new Kahoot("Ethan", 2);
-		Kahoot animalColor = new Kahoot("Branton", 10);
-		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE);
-		Kahoot presidents = new Kahoot("Obama", 44);
+		Kahoot fiftyStates = new Kahoot("Kashish", 50, "The Fifty Nify United States.");
+		Kahoot mySecondKahoot = new Kahoot("Ethan", 2, "Doubles");
+		Kahoot animalColor = new Kahoot("Branton", 10,"All the colors of the animals");
+		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE, "Everything");
+		Kahoot presidents = new Kahoot("Obama", 44, "The 44th president of the US");
 		myKahoots.add(fiftyStates);
 		myKahoots.add(mySecondKahoot);
 		myKahoots.add(animalColor);
